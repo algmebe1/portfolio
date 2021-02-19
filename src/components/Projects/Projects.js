0f1598c12c4ceab8dcd584f3709effc22d21385d
algmebe1 <algmebe1@gmail.com>
2021-02-19 16:57:37 +0100

    Projects component added

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+    return (
+    <div className="projects-container" id="projects">
+        <h2>Projects section works!</h2>
+    </div>)
+}
+
+export default Projects;
