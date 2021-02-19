cd04f02a66bd452eebc5ad7d61c32bef800c11d8
algmebe1 <algmebe1@gmail.com>
2021-02-19 16:58:01 +0100

    Contact component added

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+    return (
+    <div className="contact-container" id="contact">
+        <h2>Contact section works!</h2>
+    </div>)
+}
+
+export default Contact;
