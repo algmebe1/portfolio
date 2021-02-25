451a09fd6f8c1a57bc171d4a44dfc5f2a093d593
algmebe1 <algmebe1@gmail.com>
2021-02-25 15:46:10 +0100

    Contact component updated to jsx extension

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './Contact.css';
 
 function Contact() {
-    return (
+  return (
     <div className="contact-container" id="contact">
-        <h2>Contact section works!</h2>
-    </div>)
+      <h2>Contact section works!</h2>
+    </div>
+  );
 }
 
 export default Contact;
