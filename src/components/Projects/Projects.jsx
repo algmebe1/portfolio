f3c0b7c06b479f16c9047def60a175c6d3917708
algmebe1 <algmebe1@gmail.com>
2021-02-25 15:48:06 +0100

    Projects component updated to jsx extension

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './Projects.css';
 
 function Projects() {
-    return (
+  return (
     <div className="projects-container" id="projects">
-        <h2>Projects section works!</h2>
-    </div>)
+      <h2>Projects section works!</h2>
+    </div>
+  );
 }
 
 export default Projects;
